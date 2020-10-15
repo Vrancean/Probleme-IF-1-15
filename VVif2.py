ddead88446b14c9afb3b91aff5d147aca701b11c
Vrancean <71001705+Vrancean@users.noreply.github.com>
2020-10-15 09:18:51 +0300

    Update VVif2.py

--- VVif2.py
+++ VVif2.py
@@ -4,7 +4,7 @@ c=int(input("c="))
 if ((a<32000) and (a>0)) and ((b<32000) and (b>0)) and ((c<32000) and (b>0)):
     if ((a+b>=c) and (a+c>=b) and (b+c>=a)): 
         print("DA")
-    elif ((a+b<=c) and (a+c<=b) and (b+c<=a)): 
+    elif ((a+b<c) and (a+c<b) and (b+c<a)): 
         print("NU")
 else:
      print("Numerele nu satisfac condiția" )
